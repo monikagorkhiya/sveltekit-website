5d94ba501d37072596fd8c797379e8f31c199ea5
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-11-11 20:00:56 +0530

    feat: seo related improvement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,15 +13,15 @@
 	<link rel="icon" type="image/svg+xml" href="favicon.svg" />
 	<meta name="generator" content="Improwised" />
 	<title>Improwised Technology</title>
-	<link rel="canonical" href="/" />
-	<meta name="description" content="" />
+	<link rel="canonical" href="https://www.improwised.com/" />
+	<meta name="description" content="Let us help you with cloud infrastructure and SAAS software development services to create revenue channels. Our technical support, IT Managed Services will let you focus on your strength while we are managing your software and IT assets." />
 	<meta name="robots" content="index, follow" />
-	<meta property="og:title" content="" />
+	<meta property="og:title" content="Improwised" />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="opengraph.jpg" />
-	<meta property="og:url" content="/" />
+	<meta property="og:url" content="https://www.improwised.com/" />
 	<meta property="og:image:url" content="opengraph.jpg" />
-	<meta property="og:image:alt" content="" />
+	<meta property="og:image:alt" content="Improwised" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@improwised" />
 	<meta
