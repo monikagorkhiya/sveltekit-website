676e736a62c2e0dd65c24f3bba23eaa62b0b9332
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-11-11 19:08:09 +0530

    feat: add gh-pages for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
+
 
 export default defineConfig({
+	kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-website' : '',
+        },
+    },
+    
 	plugins: [sveltekit()],
 	build: {
 		rollupOptions:
@@ -16,3 +25,4 @@ export default defineConfig({
 		},
 	}
 });
+
